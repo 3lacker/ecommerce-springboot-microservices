f63cd7e1a6f7abc7aa0ebe9c59bbb7f3746732f9
Achraf Lakhal <160363469+3lacker@users.noreply.github.com>
2024-11-16 22:39:50 +0000

    Update GatewayConfig.java
    add inventory and customer to api gatewaye

--- api-gateway/src/main/java/com/example/api_gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/api_gateway/config/GatewayConfig.java
@@ -13,6 +13,8 @@ public class GatewayConfig {
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("product-service" ,r -> r.path("/api/products/**").uri("lb://product-service"))
+                .route("inventory-service" ,r -> r.path("/api/inventories/**").uri("lb://inventory-service"))
+                .route("customer-service" ,r -> r.path("/api/customers/**").uri("lb://customer-service"))
 
 
 
